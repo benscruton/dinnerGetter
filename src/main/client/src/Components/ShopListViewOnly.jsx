1c2806cb15e5e67f37f8aa16c45839f29db0a8c3
benscruton <benjamin.scruton@gmail.com>
2021-05-03 13:55:20 -0500

    initial commit

---
+++ src/main/client/src/Components/ShopListViewOnly.jsx
@@ -0,0 +1,14 @@
+import React from "react";
+
+const ShopListViewOnly = () => {
+
+    return (
+        <ul className="collection">
+            {
+
+            }
+        </ul>
+    );
+}
+
+export default ShopListViewOnly;
